4ba3da7f56cdfd926f868b51f1f6c01a8ac70cd0
Tommy16y <59056009+Tommy16y@users.noreply.github.com>
2020-02-01 21:17:04 +0600

    Update Exam1.java

--- Exam1.java
+++ Exam1.java
@@ -1,48 +1,24 @@
-import java.util.LinkedList;
-
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<Integer> ();
+        HashSet<Integer> integers= new HashSet<Integer> (  );
 
-        LinkedList<Person> Persons = new LinkedList<>();
-        Person person = new Person("Salim","Akhmetov","Bahtiyarovi4");
-        if (Persons.contains(person)){
-            throw  new Exception("Error");
-        }
-        else{
-            Persons.add(person);
-        }
-        Person person1 = new Person("Salim","Akhmetov","Bahtiyarovi4");
-        if (Persons.contains(person1)){
-            throw  new Exception("Мдааа");
-        }
-        else{
-            Persons.add(person1);
-        }
+        for (int i = 0; i < 100; i++) {
+            linkedList.add(i);
 
-    }
-}
-import java.util.Objects;
-public class Person {
-    String lastname;
-    String firstname;
-    String patronymic;
-    public Person(String name, String firstname, String patronymic) {
-        this.lastname = name;
-        this.firstname = firstname;
-        this.patronymic = patronymic;
-    }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return Objects.equals(lastname, person.lastname) &&
-                Objects.equals(firstname, person.firstname) &&
-                Objects.equals(patronymic, person.patronymic);
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(lastname, firstname, patronymic);
+            if(i%2==0 && i%5==0){
+                integers.add ( i );
+            }
+            else {
+
+            }
+        }
+        for (int i = 0; i <linkedList.size (); i++) {
+            if(i%2==0 && i%5!=0){
+                integers.add ( i );
+            }
+
+        }
     }
 }
